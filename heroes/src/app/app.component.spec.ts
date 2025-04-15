ca98d0178b01c236ef82da6849ab55ab98b8de44
fbarreiros <facundobarreiros@gmail.com>
2025-04-15 13:54:02 -0300

    Configuracion del app.component

--- heroes/src/app/app.component.spec.ts
+++ heroes/src/app/app.component.spec.ts
@@ -8,22 +8,11 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('Debería crear la aplicación.', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'heroes' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('heroes');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, heroes');
-  });
 });
